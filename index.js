ee776eee982b7b93dd79460938470cc05ef1b38a
swaytuned <swaytuned805@gmail.com>
2022-09-27 18:56:06 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+"use strict"
+
+let colorPicker = document.querySelector("color-picker")
+let colorBars = document.getElementById("color-bars")
+const output = document.getElementById("output")
+
+
+
+// create a function for each scheme
+// attach each scheme to said function
+// attach button to funciton callback
+
+const getColorButton = document.getElementById("get-color")
+
+getColorButton.addEventListener("click", function(){
+console.log("button clicked")
+   
+})
+
+ // Scheme
+ fetch("https://www.thecolorapi.com/scheme?hex=FF0&mode=monochrome&count=5")
+ .then( response => response.json())
+ .then( data => {
+   //  const colorArray = data.slice(0, 4);
+   //   for (let post of colorArray) {
+   //       output.innerHTML = `
+   //           <div id="color-one">${post.data[0]}</div>
+   //           <div id="color-two">${post[1]}</div>
+   //           <div id="color-three">${post[2]}</div>
+   //           <div id="color-four">${post[3]}</div>
+   //           <div id="color-five">${post[4]}</div>
+   //       `
+   //   }
+ console.log(data)
+ })
+
+ 
